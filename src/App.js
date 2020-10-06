d34c1bac4334e05551e170b88afad3da1f8598bb
huynhthaianhhd <huynhthaianhhd@gmail.com>
2020-10-06 21:20:13 +0700

    complete code

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './App.css';
+import Game from './components/Game/Game';
+
+function App() {
+  return (
+    <div className="App">
+     <Game></Game>
+    </div>
+  );
+}
+
+export default App;
